3dfaffad61df51928e2590ba1e8a6efe6cc28176
Abdul Faqih <86565657+abdulfaqih7@users.noreply.github.com>
2023-05-21 14:55:09 +0700

    Add files via upload

---
+++ js/routes.js
@@ -0,0 +1,29 @@
+
+var routes = [
+  {
+    path: '/',
+    url: './index.html',
+  },
+  {
+    path: '/about/',
+    url: './pages/about.html',
+    options: {
+      animate: true,
+    },
+  },
+  {
+    path: '/news/',
+    url: './pages/news.html',
+    options: {
+      animate: true,
+    },
+  },
+  {
+    path: '/galery/',
+    url: './pages/galery.html',
+    options: {
+      animate: true,
+    },
+  },
+  
+];
